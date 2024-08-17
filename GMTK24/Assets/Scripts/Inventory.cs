801d33f1736161e2d211f4d500087e19ed4ba4fb
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2024-08-17 17:23:24 +0200

    Growning and Shrinking pickable objects created + Inventory
    The prefabs for a growing and a shirnking mushrooms. When the player gets close can press E to pick it up and it shows in the players hand

---
+++ GMTK24/Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    private string currentlyHolding;
+    [SerializeField] private SpriteRenderer handRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentlyHolding = null;
+    }
+
+    public bool PickUpItem(string obj, Sprite sprite){
+        if(currentlyHolding != null){
+            return false;
+        }
+        currentlyHolding = obj;
+        handRenderer.sprite = sprite;
+        return true;
+    }
+
+    public void ConsumeItem(){
+        currentlyHolding = null;
+        handRenderer.sprite = null;
+    }
+}
