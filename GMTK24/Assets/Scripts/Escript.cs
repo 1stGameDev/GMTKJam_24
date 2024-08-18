7c75c8e0618344e07c4fef41d72b89c2bed889cb
Rhysghost123 <146122755+Rhysghost123@users.noreply.github.com>
2024-08-18 13:00:55 -0400

    a

--- GMTK24/Assets/Scripts/Escript.cs
+++ GMTK24/Assets/Scripts/Escript.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Escript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform Etextposition;
+    public RectTransform Etextposition;
     void Start()
     {
-        
+        Etextposition.position = new Vector3(-1000, -1000, -50);
     }
 
     // Update is called once per frame
@@ -16,4 +16,17 @@ public class Escript : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Etextposition.position = gameObject.transform.position;
+            }
+        }
+
+        private void OnTriggerExit2D (Collider2D other)
+        {
+            Etextposition.position = new Vector3(-1000, -1000, -50);
+        }
 }
